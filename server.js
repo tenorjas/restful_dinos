ffaac12e05b802d895cc0fcf56394ee6e2830c5f
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-08 15:03:26 -0400

    getting all dinos, getting individual dino by id

--- server.js
+++ server.js
@@ -12,9 +12,54 @@ app.use(bodyParser.json());
 //only accept the primitive types of data (strings, numbers, NOT arrays, NOT objects)
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// define a home page
-app.get("/", (request, response) => {
-  response.send("Hello person.");
+let allDinos = [
+  {
+    id: 1,
+    name: "Tyrannosaurus rex",
+    color: "grey",
+    weight: "8 tons",
+    habitats: ["plain", "forest"]
+  },
+  {
+    id: 2,
+    name: "Velociraptor",
+    color: "brown",
+    weight: "250 lbs",
+    habitats: ["jungle", "forest"]
+  },
+  {
+    id: 3,
+    name: "Brachiosaurus",
+    color: "brown",
+    weight: "80 tons",
+    habitats: ["plain", "forest"]
+  },
+  {
+    id: 4,
+    name: "Triceratops",
+    color: "brown",
+    weight: "15 tons",
+    habitats: ["plain", "forest"]
+  }
+];
+
+// Creating an endpoint for my API
+// this endpoint is where the back end
+// meets the front end
+// the front can see this url (/api/robot)
+// the front can see the return data as JSON
+app.get("/api/dinosaurs", (request, response) => {
+  response.json(allDinos);
+});
+
+app.get("/api/dinosaurs/:id", (request, response) => {
+  const dinoId = parseInt(request.params.id);
+  // go to my database
+  const myDino = allDinos.find(dino => {
+    return dino.id === dinoId;
+  });
+  // this is how we retrun JSON from an endpoint
+  response.json(myDino);
 });
 
 // listen on port 3000
